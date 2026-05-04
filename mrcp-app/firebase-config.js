a5e45ae53544bbfdbda383b06c0ec933a312798b
LukennyFT <lukennysoldja@hotmail.com>
2026-05-04 04:53:45 +0100

    Add files via upload

---
+++ mrcp-app/firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBEyc9yBPtKkZiBEjIPipJ5RbH1iaIC0Pk",
+  authDomain: "mrcp-revision-app.firebaseapp.com",
+  projectId: "mrcp-revision-app",
+  storageBucket: "mrcp-revision-app.firebasestorage.app",
+  messagingSenderId: "51671428714",
+  appId: "1:51671428714:web:3fd78084c36213a4672413"
+};
